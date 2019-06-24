4545e8e0cf8f7c80e06c83283903055cec7f1cfa
salishop <37633640+salishop@users.noreply.github.com>
2019-06-24 21:30:21 +0300

    Updates

---
+++ precache-manifest.f7b4013c9560103d60f6c94e13bf7183.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73e05fb405bc24a4d944c80d2bf0b10",
+    "url": "/dinamic-list-of-todo/index.html"
+  },
+  {
+    "revision": "815800739d9b74e8347d",
+    "url": "/dinamic-list-of-todo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/dinamic-list-of-todo/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "815800739d9b74e8347d",
+    "url": "/dinamic-list-of-todo/static/js/main.58c758e1.chunk.js"
+  },
+  {
+    "revision": "f4e09961efc096388bc2",
+    "url": "/dinamic-list-of-todo/static/js/runtime~main.4bca8c1d.js"
+  }
+]);
